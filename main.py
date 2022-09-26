88341fac758ed2766bcbde3606c726cae6bfbb03
rustingoff <nailunho@gmail.com>
2022-09-26 17:07:51 +0300

    git init

---
+++ main.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, Response
+from viberbot import Api
+from viberbot.api.bot_configuration import BotConfiguration
+from viberbot.api.messages import VideoMessage
+from viberbot.api.messages.text_message import TextMessage
+import logging
+
+from viberbot.api.viber_requests import ViberConversationStartedRequest
+from viberbot.api.viber_requests import ViberFailedRequest
+from viberbot.api.viber_requests import ViberMessageRequest
+from viberbot.api.viber_requests import ViberSubscribedRequest
+from viberbot.api.viber_requests import ViberUnsubscribedRequest
+
+logger = logging.getLogger()
+
+app = Flask(__name__)
+viber = Api(BotConfiguration(
+    name='Agrodata',
+    avatar='https://dl-media.viber.com/1/share/2/long/vibes/icon/image/0x0/c0b3'+
+           '/fe86971e158e03c9722d18f0f1bf1b8e9d8b7429fbc9204546734a5bf727c0b3.jpg',
+    auth_token='4fde5475b5a7e0e5-a07b269d4a6405a7-a1023d748320bb57'
+))
+
+
+@app.route('/', methods=['POST'])
+def incoming():
+    logger.debug("received request. post data: {0}".format(request.get_data()))
+    # every viber message is signed, you can verify the signature using this method
+    if not viber.verify_signature(request.get_data(), request.headers.get('X-Viber-Content-Signature')):
+        return Response(status=403)
+
+    # this library supplies a simple way to receive a request object
+    viber_request = viber.parse_request(request.get_data())
+
+    if isinstance(viber_request, ViberMessageRequest):
+        message = viber_request.message
+        # lets echo back
+        viber.send_messages(viber_request.sender.id, [
+            message
+        ])
+    elif isinstance(viber_request, ViberSubscribedRequest):
+        viber.send_messages(viber_request.get_user.id, [
+            TextMessage(text="thanks for subscribing!")
+        ])
+    elif isinstance(viber_request, ViberFailedRequest):
+        logger.warn("client failed receiving message. failure: {0}".format(viber_request))
+
+    return Response(status=200)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=443, debug=True)
