9766fc661a6c5a2f1251bfd65b3ecc880b55536f
rustingoff <nailunho@gmail.com>
2022-09-26 17:27:15 +0300

    added port

--- main.py
+++ main.py
@@ -21,32 +21,13 @@ viber = Api(BotConfiguration(
     auth_token='4fde5475b5a7e0e5-a07b269d4a6405a7-a1023d748320bb57'
 ))
 
-viber.set_webhook('https://python-viber-bot2.herokuapp.com/')
+viber.set_webhook('https://python-viber-bot2.herokuapp.com:443')
 
 
-@app.route('/', methods=['POST'])
+@app.route('/incoming', methods=['POST'])
 def incoming():
     logger.debug("received request. post data: {0}".format(request.get_data()))
-    # every viber message is signed, you can verify the signature using this method
-    if not viber.verify_signature(request.get_data(), request.headers.get('X-Viber-Content-Signature')):
-        return Response(status=403)
-
-    # this library supplies a simple way to receive a request object
-    viber_request = viber.parse_request(request.get_data())
-
-    if isinstance(viber_request, ViberMessageRequest):
-        message = viber_request.message
-        # lets echo back
-        viber.send_messages(viber_request.sender.id, [
-            message
-        ])
-    elif isinstance(viber_request, ViberSubscribedRequest):
-        viber.send_messages(viber_request.get_user.id, [
-            TextMessage(text="thanks for subscribing!")
-        ])
-    elif isinstance(viber_request, ViberFailedRequest):
-        logger.warn("client failed receiving message. failure: {0}".format(viber_request))
-
+    # handle the request here
     return Response(status=200)
 
 
